c1dab4a0fe390e2aea104f0205a29b204fecd73c
Dan Lash <danlash@gmail.com>
2012-11-12 01:29:45 -0500

    backbone model/view of notes

--- public/javascripts/plugin.js
+++ public/javascripts/plugin.js
@@ -5,11 +5,66 @@ var NodeNodes = (function(){
 	exports.App = Backbone.Model.extend({
 		start: function() {
 			console.log('started')
+
+			var notes = new List();
+			var listView = new ListView({ collection : notes, el : $('#notes') });
+			notes.fetch();
+
+			this.notes=notes;
 		}
 	});
 
 
+	var Note = Backbone.Model.extend({
+		defaults: {
+			"body" : "",
+			"closed" : false
+		}
+	});
 
+	var	List = Backbone.Collection.extend({
+		model: Note,
+		url: '/notes'
+	});
+
+	var ListView = Backbone.View.extend({
+		initialize: function(){
+			_.bindAll(this);
+			this.collection.on("reset", this.render);
+		},
+		render: function(){
+			var $notes = this.collection.map(function(note){
+				return new NoteView({ model : note }).render().el;
+			});
+
+			this.$el.empty().append($notes);
+
+			return this;
+		}
+	});
+
+	var NoteView = Backbone.View.extend({
+		tagName: "li",
+		className: "note",
+		initialize: function(){
+			_.bindAll(this);
+			this.template = _.template('\
+				<form> \
+	              <button class="btn btn-<%= buttonColor %> btn-mini"> \
+	                <b class="icon-white icon-ok"></b> \
+	              </button> \
+	            </form> \
+            	<%= Body %>');
+		},
+		render: function(){
+			var viewModel = _.extend(this.model.toJSON(), {
+				buttonColor: this.model.get('IsClosed') ? 'inverse' : 'primary'
+			});
+			this.$el.html(this.template(viewModel));
+
+			return this;
+		}
+	});
 
 	return exports;
 
---
+++ routes/note.js
@@ -0,0 +1,3 @@
+exports.list = function(req, res){
+  res.send('[{"Body":"10/24 - 10/25 ~ Matt Higgins","IsClosed":true},{"Body":"2012-11-11 ~ Dan Lash","IsClosed":false},{"Body":"Maecenas eu mi eu nisi accumsan sagittis a in augue. Duis ut orci erat, auctor rhoncus turpis. Aliquam tortor libero, eleifend eu molestie et, porttitor eu ligula. Curabitur blandit rutrum risus, quis pretium libero posuere sed. Integer adipiscing dignissim eros, id tristique leo sollicitudin auctor. Praesent in ipsum diam, sit amet iaculis lectus. Ut in diam mi. Pellentesque imperdiet suscipit odio et ultrices. Aenean blandit tellus vel orci semper euismod adipiscing augue hendrerit.","IsClosed":false},{"Body":"2012-12-31 ~ Dan Lash","IsClosed":false}]');
+};
